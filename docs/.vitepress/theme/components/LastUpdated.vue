35d1a6379c8c9d46fb96fcbf345d233c84bd2566
yangming <yangming@shinemo.com>
2022-01-22 18:34:57 +0800

    fix: 上次更新

--- docs/.vitepress/theme/components/LastUpdated.vue
+++ docs/.vitepress/theme/components/LastUpdated.vue
@@ -16,10 +16,30 @@ const prefix = computed(() => {
 })
 
 const datetime = ref('')
+const pvCount = ref()
 onMounted(() => {
   // locale string might be different based on end user
   // and will lead to potential hydration mismatch if calculated at build time
-  datetime.value = new Date(page.value.lastUpdated).toLocaleString('en-US')
+  datetime.value = new Date(page.value.lastUpdated).toLocaleString('zh-CN')
+
+  const callbackName = `cb_${Math.floor(Math.random() * 10000)}`
+  window[callbackName] = ({ pv }) => {
+    pvCount.value = pv
+  }
+  const s = document.createElement('script')
+  s.src = `https://iming.work/api/blog/count?callback=${callbackName}`
+  // s.src = `http://127.0.0.1:3000/api/blog/count?callback=${callbackName}`
+  s.referrerPolicy = 'no-referrer-when-downgrade'
+  s.defer = !0
+  s.id = 'count'
+  s.onload = () => {
+    delete window[callbackName]
+  }
+  const el = document.getElementById('count')
+  if (el) {
+    el.remove()
+  }
+  document.head.appendChild(s)
 })
 </script>
 
@@ -27,6 +47,10 @@ onMounted(() => {
   <p v-if="hasLastUpdated" class="last-updated">
     <span class="prefix">{{ prefix }}:</span>
     <span class="datetime">{{ datetime }}</span>
+  </p>
+  <p v-if="pvCount" class="last-updated" style="margin-left: 20px">
+    <span class="prefix">阅读数:</span>
+    <span class="datetime">{{ pvCount }}</span>
   </p>
 </template>
 
